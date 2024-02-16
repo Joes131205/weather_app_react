9ca62002ae0c4b43ba441508ebc65cee6e79423c
Joe Steven <86814315+Joes131205@users.noreply.github.com>
2024-02-16 23:02:25 +0700

    mobile ui dmall fr

---
+++ src/Info.jsx
@@ -0,0 +1,12 @@
+function Info() {
+    return (
+        <div className="w-52 h-52 border-5">
+            <h1 className="font-bold text-2xl">-</h1>
+            <h2 className="font-bold">-</h2>
+            <p>-</p>
+            <img src="" className="w-12 h-12" />
+        </div>
+    );
+}
+
+export default Info;
--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,20 +1,41 @@
 import { useState } from "react";
+import Info from "./Info.jsx";
 const api_key = import.meta.env.VITE_REACT_APP_WEATHER_API_KEY;
 
 function WeatherApp() {
     const [city, setCity] = useState("");
+    const [weatherInfo, setWeatherInfo] = useState({});
     function handleChange(e) {
         const value = e.target.value;
         setCity(value);
+    }
+
+    async function handleSubmit() {
         console.log(city);
+        const response = await fetch();
+        const data = await response.json();
+        
     }
     return (
-        <div>
+        <div className="w-screen h-screen flex flex-col justify-center text-center items-center gap-10">
             <h1>Weather App</h1>
-            <div>
-                <input type="text" name="city" onChange={handleChange} />
-                <input type="submit" value="Submit" />
-            </div>
+            <form
+                className="flex gap-20 items-center justify-center"
+                onSubmit={handleSubmit}
+            >
+                <input
+                    type="text"
+                    name="city"
+                    onChange={handleChange}
+                    className="px-10 py-2 border"
+                />
+                <input
+                    type="submit"
+                    value="Submit"
+                    className="px-20 py-5 border-5 text-xl "
+                />
+            </form>
+            <Info />
         </div>
     );
 }
